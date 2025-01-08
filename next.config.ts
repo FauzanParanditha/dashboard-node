0c764833826e6c6efe7893c390031df092ba88b2
Fauzan <fauzan@fellow.pandi.id>
2025-01-08 20:15:03 +0700

    remove: babel

--- next.config.ts
+++ next.config.ts
@@ -9,25 +9,6 @@ const nextConfig: NextConfig = {
     "rc-picker",
     "rc-input",
   ],
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.js$/,
-      include: [
-        /node_modules[\\/]rc-util/,
-        /node_modules[\\/]@ant-design[\\/]icons-svg/,
-        /node_modules[\\/]rc-pagination/,
-        /node_modules[\\/]rc-picker/,
-        /node_modules[\\/]rc-input/,
-      ],
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env"],
-        },
-      },
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
